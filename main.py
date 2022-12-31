d1be3de754b85611dec2115cfe3cf38a768f0ca7
SaluSam <sonate083@gmail.com>
2022-12-31 10:45:05 -0600

    Created main.py File

---
+++ main.py
@@ -0,0 +1,14 @@
+import greeting
+
+greeting.display_greeting()
+
+fav_pokemon = input("What is your favorite Pokemon")
+
+
+for character in fav_pokemon:
+    print(character)
+    if character == ("c"):
+        break
+    
+
+
